1967cc07b56bfb58f52db1ea832df42630aa44ae
Adam Gall <adam@gall.io>
2015-06-14 22:17:52 -0400

    Re-generate Item and List NSManagedObject subclasses in order for Xcode to generate the property "addItemsObject", "removeItemsObject", "addItems", and "removeItems" methods to List.h.

--- LittleToDoApp/Item.h
+++ LittleToDoApp/Item.h
@@ -2,17 +2,18 @@
 //  Item.h
 //  LittleToDoApp
 //
-//  Created by Marty Lavender on 6/14/15.
+//  Created by Adam Gall on 6/14/15.
 //  Copyright (c) 2015 Marty Lavender. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 #import <CoreData/CoreData.h>
 
+@class List;
 
 @interface Item : NSManagedObject
 
 @property (nonatomic, retain) NSString * itemName;
-
+@property (nonatomic, retain) List *list;
 
 @end
--- LittleToDoApp/List.m
+++ LittleToDoApp/List.m
@@ -2,14 +2,17 @@
 //  List.m
 //  LittleToDoApp
 //
-//  Created by Marty Lavender on 6/14/15.
+//  Created by Adam Gall on 6/14/15.
 //  Copyright (c) 2015 Marty Lavender. All rights reserved.
 //
 
 #import "List.h"
+#import "Item.h"
+
 
 @implementation List
 
 @dynamic listName;
+@dynamic items;
 
 @end
